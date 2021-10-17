97b8ba344e553ed7040bcc1ea04006eeab5e9aa5
Brianna Doubt <bri@briannadoubt.com>
2021-10-16 19:35:03 -0700

    Fix package name comment, fix visibility, add previews

--- Sources/FireUI/FireContentView.swift
+++ Sources/FireUI/FireContentView.swift
@@ -1,34 +1,35 @@
 //
 //  FireContentView.swift
-//  Twerking Girl
+//  FireUI
 //
-//  Created by Bri on 10/15/21.
+//  Created by Brianna Doubt on 10/15/21.
 //
 
 import SwiftUI
 
-@available(iOS 13.0, macOS 10.15, *)
-struct FireContentView<Human: Person, Content: View>: View {
+public struct FireContentView<Human: Person, Content: View>: View {
     
     @EnvironmentObject private var user: FirebaseUser
     @ObservedObject private var person: FirestoreDocument<Human>
     
     private let content: Content
     
-    init(personBasePath: String, uid: PersonID, @ViewBuilder content: @escaping () -> Content) {
+    public init(personBasePath: String, uid: PersonID, @ViewBuilder content: @escaping () -> Content) {
         self.person = FirestoreDocument(collection: personBasePath , id: uid)
         self.content = content()
     }
     
-    var body: some View {
+    public var body: some View {
         content
             .observe(person)
             .environmentObject(person)
     }
 }
 
-//struct FireContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        FireContentView()
-//    }
-//}
+struct FireContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        FireContentView<DemoPerson, DemoContentView>(personBasePath: "users", uid: "test") {
+            DemoContentView()
+        }
+    }
+}
