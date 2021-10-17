cdb8567109c07aa345ea5f663ace02654f413284
Brianna Doubt <bri@briannadoubt.com>
2021-10-16 19:34:19 -0700

    Update author, fix visibility, move preview data

--- Sources/FireUI/FireClient.swift
+++ Sources/FireUI/FireClient.swift
@@ -2,30 +2,27 @@
 //  FireClient.swift
 //  FireUI
 //
-//  Created by Brianna Zamora on 10/9/21.
+//  Created by Brianna Doubt on 10/9/21.
 //
 
 import SwiftUI
 import Firebase
 import FirebaseFirestoreSwift
 
-@available(iOS 15.0.0, *)
-struct FireClient<Human: Person>: ViewModifier {
-    func body(content: Content) -> some View {
+public struct FireClient<Human: Person>: ViewModifier {
+    public func body(content: Content) -> some View {
         Client<Human, Content> {
             content
         }
     }
 }
 
-@available(iOS 15.0, *)
 extension View {
-    func onFire<Human: Person>(_ personType: Human.Type) -> some View {
+    public func onFire<Human: Person>(_ personType: Human.Type) -> some View {
         modifier(FireClient<Human>())
     }
 }
 
-@available(iOS 15.0.0, *)
 public struct Client<Human: Person, Content: View>: View {
 
     @StateObject public var user: FirebaseUser
@@ -49,7 +46,6 @@ public struct Client<Human: Person, Content: View>: View {
         self.authenticationView = authenticationView()
     }
 
-    @available(iOS 15.0, *)
     public var body: some View {
         Group {
             if user.isAuthenticated, let uid = user.uid {
@@ -73,50 +69,8 @@ extension Bundle {
     }
 }
 
-#if DEBUG
-@available(macOS 10.15, iOS 14.0, *)
 struct FireClient_Previews: PreviewProvider {
     static var previews: some View {
-        PreviewContentView().onFire(PreviewPerson.self)
+        DemoContentView().onFire(DemoPerson.self)
     }
 }
-
-struct PreviewContentView: View {
-    var body: some View {
-        Text("Hello, world!").padding()
-    }
-}
-
-struct PreviewPerson: Person {
-    
-    @DocumentID var id: PersonID?
-    
-    var nickname = "Meowface"
-    var role: PreviewRole?
-    var email: String = "meow@meow.com"
-    
-    var created: Date
-    var updated: Date
-    
-    func basePath() -> String {
-        "users"
-    }
-    
-    static func new(uid: PersonID, email: String, nickname: String) -> PreviewPerson {
-        PreviewPerson(
-            id: uid,
-            nickname: nickname,
-            role: nil,
-            email: email,
-            created: Date(),
-            updated: Date()
-        )
-    }
-}
-
-enum PreviewRole: String, PersonRole {
-    case developer
-    var id: String { rawValue }
-    var title: String { rawValue }
-}
-#endif
