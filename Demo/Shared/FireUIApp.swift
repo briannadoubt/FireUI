bed5a3b6f070bd97526383173592239e86913860
Brianna Doubt <bri@briannadoubt.com>
2021-11-19 14:48:00 -0800

    Updated FireScene implementation, added comments for template

--- Demo/Shared/FireUIApp.swift
+++ Demo/Shared/FireUIApp.swift
@@ -6,16 +6,20 @@
 //
 
 import FireUI
-import SwiftUI
 
 @main
 struct FireUIApp: App {
     
     var body: some Scene {
         // Wrap FireScene around ContentView
-        FireScene(stateType: FireUIAppState.self, newPerson: Human.new) {
+        FireScene(
+            state: FireUIAppState.self,
+            product: FireUIProduct.self,
+            newPerson: Human.new
+        ) {
             // This is your run of the mill ContentView
             ContentView()
+            
         } logo: {
             // The logo image is displayed on the AuthenticationView, the SettingsView, and at the top of the Sidebar on macOS.
             // By default this template has loaded an image named "logo" in the Assets catelog and references it below.
@@ -23,9 +27,10 @@ struct FireUIApp: App {
             Image("logo")
                 .resizable()
                 .foregroundColor(Color("AccentColor"))
+            
         } settings: {
-            // TODO: Make SettingsView customizable
             SettingsView()
+            
         } footer: {
             // The footer is displayed at the bottom of the AuthenticationView and the SettingsView.
             // Delete this parameter to remove the footer from your app.
