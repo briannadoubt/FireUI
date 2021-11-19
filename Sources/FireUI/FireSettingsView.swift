c85d7b7bd67f11c8874d0e5a370334a0d11e944e
Brianna Doubt <bri@briannadoubt.com>
2021-11-19 14:51:03 -0800

    Add Settings

---
+++ Sources/FireUI/FireSettingsView.swift
@@ -0,0 +1,35 @@
+//
+//  FireSettingsView.swift
+//  FireUI
+//
+//  Created by Brianna Doubt on 11/19/21.
+//
+
+@_exported import SwiftUI
+
+public struct FireSettingsView<Human: Person, Settings: View>: View {
+    
+    @EnvironmentObject private var user: FirebaseUser
+    @StateObject private var person: FirestoreDocument<Human>
+    
+    @ViewBuilder private let settings: () -> Settings
+    
+    public init(uid: PersonID, @ViewBuilder settings: @escaping () -> Settings) {
+        self._person = StateObject(wrappedValue: FirestoreDocument<Human>(collection: Human.basePath(), id: uid))
+        self.settings = settings
+    }
+    
+    public var body: some View {
+        AnyView {
+            settings()
+                .observe(person)
+                .environmentObject(person)
+        }
+    }
+}
+
+//struct FireSettingsView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        FireSettingsView<<#Human: Person#>, <#Settings: View#>>()
+//    }
+//}
